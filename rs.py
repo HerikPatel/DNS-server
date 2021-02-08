b2da1c4dfac5b5853155ddc86971bf2c24e4f914
Herik Patel <67105093+HerikPatel@users.noreply.github.com>
2021-02-08 11:49:35 -0500

    Add files via upload

--- rs.py
+++ rs.py
@@ -17,8 +17,8 @@ def get_DNS_values():  # Gets values of dns table and stores in dictonary
     return rs_DNS
 
 
+# Connects with client and recives and sends data to the client
 def check_DNS_table(port, rs_dns):
-
     ip = 'localhost'
     rs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_binding = (ip, port)
@@ -26,23 +26,23 @@ def check_DNS_table(port, rs_dns):
     rs.listen(3)
     print("waiting for connection")
     conn, addr = rs.accept()
-    while True:
-
-        data_from_client = conn.recv(100)
-        if rs_dns.get(data_from_client):
-            msg = "" + data_from_client + " " + \
-                rs_dns[data_from_client][0]+" "+rs_dns[data_from_client][1]
-        else:
-            msg = "" + rs_dns['Error 404'][0]+" "+rs_dns['Error 404'][1]
-        print(data_from_client)
-        conn.send(msg.encode('utf-8'))
-
-    conn.close()
+    try:
+        while True:
+            data_from_client = conn.recv(100)
+            if rs_dns.get(data_from_client):
+                msg = "" + data_from_client + " " + \
+                    rs_dns[data_from_client][0]+" "+rs_dns[data_from_client][1]
+            else:
+                msg = "" + rs_dns['Error 404'][0]+" "+rs_dns['Error 404'][1]
+            # print(data_from_client)
+            conn.send(msg.encode('utf-8'))
+    except:
+        conn.close()
 
 
 if __name__ == "__main__":
     if(len(sys.argv) != 2):
-        #rs_port = int(sys.argv[1])
+        # rs_port = int(sys.argv[1])
         rs_dns = get_DNS_values()
         check_DNS_table(28000, rs_dns)
     else:
