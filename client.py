ee480cbc5a2f65469928ef55fe7bb469af2ea597
Herik Patel <67105093+HerikPatel@users.noreply.github.com>
2021-02-08 09:51:47 -0500

    Client and server can read from given files

--- client.py
+++ client.py
@@ -1,11 +1,21 @@
 import socket
+import sys
+f = open("PROJI-HNS.txt", "r")
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-port = 2080  # Port number will be provided in command line interface this is temp
+port = 28000  # Port number will be provided in command line interface this is temp
 client.connect(('localhost', port))
-msg = "www.netflix.com"  # From command line
-client.send(msg.encode('utf-8'))
-data_from_server = client.recv(100)
-if "NS" in data_from_server:
-    print("Forward request to TS server")
-print(data_from_server.decode('utf-8'))
+for x in f:
+    x = x.replace('\n', '')
+    x = x.lower()
+    if x != "\n":
+        msg = ""+x
+        # print(msg)
+        client.send(msg.encode('utf-8'))
+        data_from_server = ""
+        data_from_server = client.recv(100)
+        if "NS" in data_from_server:
+            print("Forward request to TS server")
+        print(data_from_server.decode('utf-8'))
+        msg = ""
 client.close()
--- rs.py
+++ rs.py
@@ -1,33 +1,46 @@
 import socket
+import sys
 
-rs_dns = {
-    "qtsdatacenter.aws.com": ['128.64.3.2', 'A'],
-    "kill.cs.rutgers.edu": ['182.48.3.2', 'A'],
-    "mx.rutgers.edu": ['192.64.4.2', 'A'],
-    "www.ibm.com": ['64.42.3.4', 'A'],
-    "www.google.com": ['8.6.4.2', 'A'],
-    "Error 404": ['localhost', 'NS']
-}
+
+def get_DNS_values():
+    rs_DNS = {}
+    f = open("PROJI-DNSRS.txt", "r")
+    for x in f:
+        temparr = x.split()
+        domain_name = temparr[0]
+        ip_address = temparr[1]
+        flag = temparr[2]
+        if ip_address != '-':
+            rs_DNS.update({domain_name: [ip_address, flag]})
+        else:
+            rs_DNS.update({"Error 404": [domain_name, flag]})
+
+#    print(rs_DNS)
 
 
 def check_DNS_table(domain_name):
+
     ip = 'localhost'
-    port = 2080  # Port number will be provided in command line interface this is temp
+    port = 28000  # Port number will be provided in command line interface this is temp
     rs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_binding = (ip, port)
     rs.bind(server_binding)
-    rs.listen(1)
+    rs.listen(3)
     print("waiting for connection")
     conn, addr = rs.accept()
-    data_from_client = conn.recv(100)
-    if rs_dns.get(data_from_client):
-        msg = "" + data_from_client + " " + \
-            rs_dns[data_from_client][0]+" "+rs_dns[data_from_client][1]
-    else:
-        msg = "" + rs_dns['Error 404'][0]+" "+rs_dns['Error 404'][1]
-    print(data_from_client)
-    conn.send(msg.encode('utf-8'))
+    while True:
+
+        data_from_client = conn.recv(100)
+        if rs_dns.get(data_from_client):
+            msg = "" + data_from_client + " " + \
+                rs_dns[data_from_client][0]+" "+rs_dns[data_from_client][1]
+        else:
+            msg = "" + rs_dns['Error 404'][0]+" "+rs_dns['Error 404'][1]
+        print(data_from_client)
+        conn.send(msg.encode('utf-8'))
+
     conn.close()
 
 
-check_DNS_table("")
+# check_DNS_table("")
+get_DNS_values()
